ccabfcc035f04af514d5d8dead0dd31a2ecf8ba0
tingle2008 <tingle2008@gmail.com>
2016-04-16 21:39:45 +0800

    init

---
+++ scripts/preun.sh
@@ -0,0 +1,10 @@
+if [ $1 = 0 ] ; then
+    if [ -f /etc/init/svscanboot.conf ];then
+	initctl stop svscanboot
+	rm -rf /etc/init/svscanboot.conf
+    fi
+    grep -q svscanboot /etc/inittab || exit 0
+    mv -f /etc/inittab /etc/inittab.tmp.$$
+    sed /svscanboot/d </etc/inittab.tmp.$$ >/etc/inittab && rm /etc/inittab.tmp.$$
+fi
+init q
